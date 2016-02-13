7a8480f2deb79467b41b015849fb210424f11907
jpwall <jessephilipwalling@gmail.com>
2016-02-13 10:43:27 -0800

    fix unchanging price bug, fix not notifying when to sell, reorganize code

--- stock.js
+++ stock.js
@@ -3,25 +3,37 @@ var jsdom = require('jsdom');
 // -q for quickbuy program
 if (process.argv[2] == '-q') {
     var stock = process.argv[3];
-    var inc = process.argv[4];
+    var inc = Number(process.argv[4]);
+    
     jsdom.env('http://www.nasdaq.com/symbol/' + stock + '/real-time', function(err, window) {
-	function getStockPrice() {
-	    var priceDollar = window.document.getElementsByClassName('qwidget-dollar')[0].textContent;
-	    var price = priceDollar.split('$');
-	    console.log(price[1]);
-	    return price[1];
+	if (err) {
+	    console.log(err);
 	}
-	
-	var oldPrice = getStockPrice();
-	function calcSell() {
-	    var updPrice = getStockPrice();
-	    if (updPrice >= (oldPrice + inc)) {
-		console.log('SELL AT: ' + updPrice);
-		clearInterval(interval);
+	else {
+	    var priceDollarOld = window.document.getElementsByClassName('qwidget-dollar')[0].textContent;
+	    var priceSplitOld = priceDollarOld.split('$');
+	    var priceOld = Number(priceSplitOld[1]);
+		
+	    function calculateStock() {
+		var priceDollarNew = window.document.getElementsByClassName('qwidget-dollar')[0].textContent;
+		var priceSplitNew = priceDollarNew.split('$');
+		var priceNew = Number(priceSplitNew[1]);
+		    
+		if (priceNew >= (priceOld + inc)) {
+		    console.log('SELL AT: ' + priceNew);
+		    clearInterval(interval);
+		    //window.close();
+		}
+		else {
+		    console.log(priceNew);
+		    //window.close();
+		}
 	    }
+	    
+	    var interval = setInterval(calculateStock, 5000);
 	}
-	var interval = setInterval(calcSell, 5000);
     });
+	
 }
 
 // -c for calculate program
@@ -31,14 +43,14 @@ if (process.argv[2] == '-c') {
     var shares = process.argv[5];
     var charge = process.argv[6];
     function getProfit(buy, sell, stockShares, deduct) {
-	dsplit = deduct.split('%');
-	if (dsplit[1] == '') {
-	    var percent = parseInt(dsplit[0]);
+	dSplit = deduct.split('%');
+	if (dSplit[1] == '') {
+	    var percent = parseInt(dSplit[0]);
 	    var profit = ((sell - buy) * stockShares) * (-(percent * 0.01) + 1);
 	    console.log(profit);
 	}
 	else {
-	    var profit = (((sell - buy) * stockShares) - deduct);
+	    profit = (((sell - buy) * stockShares) - deduct);
 	    console.log(profit);
 	}
     }
